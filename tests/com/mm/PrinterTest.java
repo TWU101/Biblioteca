5f592d43b159ffafc37d92962b0f4f8a36901975
mm <mauricio.machicado@gmail.com>
2016-04-08 16:55:11 -0500

    Added Printer test with test for printMessage()

---
+++ tests/com/mm/PrinterTest.java
@@ -0,0 +1,28 @@
+package com.mm;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class PrinterTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    Printer printer = new Printer();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void shouldPrintTestMessage() {
+        printer.printMessage("test");
+        assertThat(outContent.toString(), is("test\n"));
+    }
+
+}
